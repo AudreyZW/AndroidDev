446c1061831700d02482e90f33d6bd8f5c65f0f8
AudreyZW <audrey.ziwei.hu@gmail.com>
2018-10-29 01:12:36 -0400

    Basic UI of navigation + a bit dev for Note
    Navigation menus, toolbar stucture finished.
    Tested out an simple implementation of fragment in Notes.

---
+++ DoneDone/app/src/main/java/audreyhu/donedone/fragments/NoteFragment.java
@@ -0,0 +1,23 @@
+package audreyhu.donedone.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import audreyhu.donedone.R;
+
+
+public class NoteFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.note_main, container, false);
+
+
+    }
+}
